f539a33f66d9dc258d830a8441047820c76d9621
HellCat46 <hellcat2388@gmail.com>
2023-10-08 14:59:14 +0530

    Implemented Placeholder

--- Xiaoshi/Report.Designer.cs
+++ Xiaoshi/Report.Designer.cs
@@ -33,9 +33,9 @@
             this.issueDesc = new System.Windows.Forms.RichTextBox();
             this.privacy_chkbox = new System.Windows.Forms.CheckBox();
             this.btnsbmt = new System.Windows.Forms.Button();
-            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
             this.lblname = new System.Windows.Forms.Label();
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.reportermail = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // issueTitle
@@ -44,6 +44,8 @@
             this.issueTitle.Name = "issueTitle";
             this.issueTitle.Size = new System.Drawing.Size(292, 20);
             this.issueTitle.TabIndex = 1;
+            this.issueTitle.Enter += new System.EventHandler(this.issueTitle_Enter);
+            this.issueTitle.Leave += new System.EventHandler(this.issueTitle_Leave);
             // 
             // issueDesc
             // 
@@ -52,6 +54,8 @@
             this.issueDesc.Size = new System.Drawing.Size(292, 127);
             this.issueDesc.TabIndex = 2;
             this.issueDesc.Text = "";
+            this.issueDesc.Enter += new System.EventHandler(this.issueDesc_Enter);
+            this.issueDesc.Leave += new System.EventHandler(this.issueDesc_Leave);
             // 
             // privacy_chkbox
             // 
@@ -72,13 +76,6 @@
             this.btnsbmt.UseVisualStyleBackColor = true;
             this.btnsbmt.Click += new System.EventHandler(this.btnsbmt_Click);
             // 
-            // maskedTextBox1
-            // 
-            this.maskedTextBox1.Location = new System.Drawing.Point(12, 29);
-            this.maskedTextBox1.Name = "maskedTextBox1";
-            this.maskedTextBox1.Size = new System.Drawing.Size(292, 20);
-            this.maskedTextBox1.TabIndex = 0;
-            // 
             // lblname
             // 
             this.lblname.Location = new System.Drawing.Point(12, 9);
@@ -87,13 +84,20 @@
             this.lblname.TabIndex = 6;
             this.lblname.Text = "Your Email:";
             // 
+            // reportermail
+            // 
+            this.reportermail.Location = new System.Drawing.Point(12, 29);
+            this.reportermail.Name = "reportermail";
+            this.reportermail.Size = new System.Drawing.Size(292, 20);
+            this.reportermail.TabIndex = 7;
+            // 
             // Report
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(316, 289);
+            this.Controls.Add(this.reportermail);
             this.Controls.Add(this.lblname);
-            this.Controls.Add(this.maskedTextBox1);
             this.Controls.Add(this.btnsbmt);
             this.Controls.Add(this.privacy_chkbox);
             this.Controls.Add(this.issueDesc);
@@ -114,8 +118,8 @@
         private System.Windows.Forms.RichTextBox issueDesc;
         private System.Windows.Forms.CheckBox privacy_chkbox;
         private System.Windows.Forms.Button btnsbmt;
-        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
         private System.Windows.Forms.Label lblname;
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.TextBox reportermail;
     }
 }
